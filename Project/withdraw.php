78086448cd1345048c73c87d68b2b7e7a31ab79d
Minh Le <qminh279@gmail.com>
2023-12-14 00:44:17 -0500

    implemented withdrawal functionality

--- Project/withdraw.php
+++ Project/withdraw.php
@@ -14,7 +14,7 @@ is_logged_in(true);
                     $accounts = $_SESSION["user"]["accounts"];
                     foreach ($accounts as $account) {
                         $optionText = $account['account_number'] . ' (Balance: $' . $account['balance'] . ')';
-                        echo "<option value='{$account['balance']}'>{$optionText}</option>";
+                        echo "<option value='{$account['id']}'>{$optionText}</option>";
                     }
                 ?>
             </select>
@@ -99,11 +99,14 @@ is_logged_in(true);
 <script>
     function validate(form) {
         const withdrawAmount = parseInt(form.withdrawAmount.value);
-        const accountBalance = parseInt(document.getElementById("accountSelect").value);
-        
+        const accountSelectElement = document.getElementById("accountSelect");
+        const selectedOption = accountSelectElement.options[accountSelectElement.selectedIndex];
+        const balanceText = selectedOption.textContent.match(/\$\d+/)[0];
+        const accountBalance = parseInt(balanceText.substring(1));
+
         let isValid = true;
         if (!withdrawAmount || withdrawAmount <= 0) {
-            flash("Deposit amount must be greater than 0 or not empty", "warning");
+            flash("Withdraw amount must be greater than 0 or not empty", "warning");
             isValid = false;
         }
         if (!isValid) return false;
@@ -115,6 +118,73 @@ is_logged_in(true);
         return isValid;
     }
 </script>
+
+<?php
+if (isset($_POST["accountSelect"]) && isset($_POST["withdrawAmount"])) {
+    $account_id = se($_POST, "accountSelect", "", false);
+    $withdrawAmount = se($_POST, "withdrawAmount", "", false);
+    $memo = se($_POST, "memo", "", false);
+    $account = has_account($account_id);
+    // validate
+    $hasError = false;
+    if (empty($account_id)) {
+        flash("Account must not be empty", "danger");
+        $hasError = true;
+    }
+    if (empty($withdrawAmount)) {
+        flash("withdraw amount must not be empty", "danger");
+        $hasError = true;
+    }
+    if (!$account) {
+        flash("You don't have permission", "danger");
+        $hasError = true;
+    }
+    if ($account["balance"] < $withdrawAmount) {
+        flash("Withdraw amount exceeds balance", "danger");
+        $hasError = true;
+    }
+    if (!$hasError) {
+        $db = getDB();
+        try {
+            // Fetch the current balance of each account
+            $stmt = $db->prepare("SELECT balance FROM Accounts WHERE id = :account_id");
+            $stmt->execute([":account_id" => -1]);
+            $world_acc = $stmt->fetch(PDO::FETCH_ASSOC);
+            $world_acc_balance = $world_acc["balance"];
+
+            $stmt->execute([":account_id" => $account_id]);
+            $user_acc = $stmt->fetch(PDO::FETCH_ASSOC);
+            $user_acc_balance = $user_acc["balance"];
+
+            $world_expected_total = $world_acc_balance + $withdrawAmount;
+            $user_expected_total = $user_acc_balance - $withdrawAmount;
+
+            // Create transaction pair
+            $stmt = $db->prepare("INSERT INTO Transactions (account_src, account_dest, balance_change, transaction_type, memo, expected_total) 
+                                VALUES(:account_src, :account_dest, :balance_change, :transaction_type, :memo, :expected_total)");
+            $stmt->execute([":account_src" => $account_id, ":account_dest" => -1, ":balance_change" => -$withdrawAmount, ":transaction_type" => "withdraw", ":memo" => $memo, ":expected_total" => $user_expected_total]);
+            $stmt->execute([":account_src" => -1, ":account_dest" => $account_id, ":balance_change" => $withdrawAmount, ":transaction_type" => "withdraw", ":memo" => $memo, ":expected_total" => $world_expected_total]);
+
+            // Update account balances
+            $stmt = $db->prepare("UPDATE Accounts SET balance = (SELECT SUM(balance_change) FROM Transactions WHERE account_src = :account_id) WHERE id = :account_id");
+            $stmt->execute([":account_id" => $account_id]);
+            $stmt->execute([":account_id" => -1]);
+
+            // renew session
+            $stmt = $db->prepare("SELECT * FROM Accounts WHERE user_id = :user_id");
+            $stmt->execute([":user_id" => get_user_id()]);
+            $updated_accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $_SESSION["user"]["accounts"] = $updated_accounts;
+
+            flash("Successfully withdrawed!", "success");
+            die(header("Location: " . get_url("my_accounts.php")));
+        } catch (Exception $e) {
+            flash("An error occurred while withdrawing", "danger");
+            error_log(var_export($e, true));
+        }
+    }
+}
+?>
 
 <?php
 require(__DIR__ . "/partials/flash.php");
